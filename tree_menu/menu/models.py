c8987539c142c47ff1a9c9550381977c168fabef
Dmitriy-boytsev <141399911+Dmitriy-boytsev@users.noreply.github.com>
2024-07-24 16:16:21 +0300

    fix models and add docstring

--- tree_menu/menu/models.py
+++ tree_menu/menu/models.py
@@ -1,28 +1,22 @@
 from django.db import models
 
 
-class BaseModel(models.Model):
-    """Абстрактный базовый класс с общими методами."""
-    class Meta:
-        abstract = True
-
-    def __str__(self):
-        """Возвращает строковое представление объекта."""
-        return str(self.name)
-
-
-class Menu(BaseModel):
+class Menu(models.Model):
     """Представляет меню."""
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        """Возвращает строковое представление объекта меню."""
+        return self.name
 
-class MenuItem(BaseModel):
+
+class MenuItem(models.Model):
     """Представляет элемент меню."""
     menu = models.ForeignKey(Menu, related_name="items", on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=100, blank=True, null=True)
-    named_url = models.CharField(max_length=200, blank=True, null=True)
-    parent = models.ForeignKey("self", blank=True, null=True, related_name="children", on_delete=models.CASCADE)
+    named_url = models.CharField(max_length=200,blank=True, null=True)
+    parent = models.ForeignKey("self", blank=True, null=True, related_name="children",on_delete=models.CASCADE)
 
     def get_url(self):
         """Возвращает URL элемента меню."""
@@ -31,3 +25,7 @@ class MenuItem(BaseModel):
         elif self.named_url:
             return reversed(self.named_url)
         return "#"
+    
+    def __str__(self):
+        """Возвращает строковое представление объекта меню."""
+        return self.name
